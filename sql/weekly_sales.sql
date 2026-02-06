06d5bd0570d9da7eae69e874754450b2469a4715
eli-10-dev <elijah.gaylan@gmail.com>
2026-02-06 18:59:37 +0800

    Truncated from extracted week and filtered out days from 2013 instead of extracting the year from the actual date column.

--- sql/weekly_sales.sql
+++ sql/weekly_sales.sql
@@ -1,14 +1,15 @@
 WITH cte AS (
 	SELECT
 		DATE_TRUNC('week', date) AS week_start,
-		EXTRACT('year' FROM date) AS year,
 		ROUND(AVG(sales), 2) AS avg_sales
 	FROM food_sales
-	GROUP BY week_start, year
-	ORDER BY week_start
+	WHERE date >= DATE '2014-01-01'
+	GROUP BY week_start
 )
 SELECT
-	year,
-	EXTRACT('week' FROM week_start) AS week,
+	EXTRACT(year FROM week_start) AS year,
+	EXTRACT(week FROM week_start) AS week,
 	avg_sales
-FROM cte;
+FROM cte
+WHERE EXTRACT(year FROM week_start) >= 2014
+ORDER BY year, week;
